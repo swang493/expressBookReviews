6509a1921ab349b6a1083d43e9c7510bb564209d
wangsa <wangsa19960109@gmail.com>
2023-09-30 20:03:51 -0400

    promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,34 +19,71 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-    //Write your code here
-    res.send(JSON.stringify({ books }, null, 4));
-    //return res.status(300).json({message: "Yet to be implemented"});
+    let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Promise resolved")
+        }, 2000)
+    })
+
+    myPromise.then((successMessage) => {
+        console.log("callback " + successMessage)
+        res.send(JSON.stringify({ books }, null, 4));
+    })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-    const book_need = req.params.isbn;
-    res.send(books[book_need]);
+    let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Promise resolved")
+        }, 2000)
+    })
 
+    myPromise.then((successMessage) => {
+        console.log("callback " + successMessage)
+        let isbn = req.params.isbn;
+        res.send(books[isbn]);
+
+    })
+
+    console.log("After calling promise");
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-    const authors = req.params.author;
-    const book_arrs = Object.values(books);
+    let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Promise resolved ")
+        }, 2000)
+    })
 
-    const book = book_arrs.filter((book) => book.author === authors);
-    res.status(200).json(book);
-
+    myPromise.then((successMessage) => {
+        console.log("callback " + successMessage)
+        const authors = req.params.author;
+        const book_arrs = Object.values(books);
+        const book = book_arrs.filter((book) => book.author === authors);
+        res.status(200).json(book);
+    })
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-    const title = req.params.title;
-    const book_arr = Object.values(books);
-    const book = book_arr.filter((book) => book.title === title);
-    res.status(200).json(book);
+    let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Promise resolved in getting book details based on title")
+        }, 2000)
+    })
+
+    //Console log before calling the promise
+    console.log("Before calling promise");
+    //Call the promise and wait for it to be resolved and then print a message.
+    myPromise.then((successMessage) => {
+        console.log("From Callback " + successMessage)
+        const title = req.params.title;
+        const book_arr = Object.values(books);
+        const book = book_arr.filter((book) => book.title === title);
+        res.status(200).json(book);
+    })
 });
 
 //  Get book review
